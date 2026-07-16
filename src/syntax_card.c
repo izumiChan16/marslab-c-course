fca1a57c2b4b08e4ba542f6593b6b187dd9a1e96
wezaaripooto <onlyforkc001@gmail.com>
2026-07-16 21:05:28 +0800

    任务1 2026.07.16

---
+++ src/syntax_card.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    // 从这里开始输出
+    printf("Course: %c\n", 'C');
+    printf("Task: %s\n", "Basic Syntax");
+    printf("Exercises: %i\n", 3);
+    printf("Status: %s\n","ready");
+    /*在这里结束 */
+    return 0;
+}
