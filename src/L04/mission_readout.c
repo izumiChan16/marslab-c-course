557f4ce1554aa21064acd4fa96e30b0f1500b320
wezaaripooto <onlyforkc001@gmail.com>
2026-07-25 21:29:53 +0800

    有返回值的函数 (#17)
    * 有返回值的函数
    
    * 增加了返回值以及修改了一个问题

---
+++ src/L04/mission_readout.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int range(int a)
+{   
+    if (a<0 || a>100){
+        printf("格式错误\n");
+        return -1;
+    }
+    return 0;
+
+}
+
+int energy(int distance,int cargo)
+{
+    int consumption;
+    consumption = distance*4 + cargo*2;
+    return consumption;
+}
+int ready(int consumption,int sigbal);
+
+
+int grade(int signal,int consumption)
+{
+    if(consumption<=500 && signal>=60)
+    {
+        if(signal >= 85 && consumption <=150)
+        {
+            printf("等级为%c\n",'S');
+        }
+        else
+        {
+            printf("等级为%c\n",'A');
+        }
+    }
+    else
+    {
+        printf("等级为%c\n",'C');
+    }
+    return 0;
+}
+
+int main()
+{
+    int distance_km, cargo_kg, signal,input;
+
+    input = scanf("%d %d %d",&distance_km,&cargo_kg,&signal);
+    if (input != 3){
+        printf("格式错误\n");
+        return -1;
+    }
+
+    if(range(distance_km) == -1 || range(cargo_kg) == -1 || range(signal) == -1) 
+    {
+    return -1;
+    }
+
+    // ------------------------
+    int consumption = energy(distance_km, cargo_kg);
+    
+    ready(consumption,signal);
+    // --------------------------
+
+    grade(signal,consumption);
+
+    return 0;
+}
+
+int ready(int consumption,int signal)
+{
+    if(consumption<=500 && signal>=60){
+        printf("能耗为%d, ",consumption);
+        printf("就绪, ");  
+    }
+    else{
+        printf("未就绪, ");
+    }
+    return 0;
+}
